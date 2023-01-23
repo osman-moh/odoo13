842e57a20a45f733150fc8029d06487bf7ba6a3d
osman-moh <osmanmoh119@gmail.com>
2023-01-23 10:11:07 +0300

    test update

--- dynamic_accounts_report/__manifest__.py
+++ dynamic_accounts_report/__manifest__.py
@@ -21,7 +21,7 @@
 #############################################################################
 
 {
-    'name': 'Dynamic Financial Reports',
+    'name': 'Dynamic Financial Reports Era',
     'version': '15.0.1.1.5',
     'category': 'Accounting',
     'live_test_url': 'https://www.youtube.com/watch?v=gVQi9q9Rs-E&t=5s',
